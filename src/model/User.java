a06737dea043472f18cd7dada570d5517712707f
Lucas Levandoski <lucasdelucas124@gmail.com>
2023-10-17 21:35:57 -0300

    Criando método addOrder

--- src/model/User.java
+++ src/model/User.java
@@ -16,6 +16,18 @@ public class User {
         this.department=department;
     }
 
+public static boolean addOrder (Order o){
+        if (index > orders.length)
+            throw new ArrayIndexOutOfBoundsException("Amount of orders has been exceeded by user. Please remove an existing order and try again. ");
+        else {
+            orders[index]=o;
+            index++;
+            return true;
+        }
+    }
+
+
+
 public String getFirst_name() {
 
         return first_name;
