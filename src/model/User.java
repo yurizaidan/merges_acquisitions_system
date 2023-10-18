6d1d9b04fde8a9dbeced651c8758ae4f8745467b
Lucas Levandoski <lucasdelucas124@gmail.com>
2023-10-17 21:42:44 -0300

    Adicionado novo método createOrder

--- src/model/User.java
+++ src/model/User.java
@@ -6,3 +6,23 @@ public static boolean addOrder(Order o){
             index++;
             return true;
         }
+
+public static boolean createOrder() throws ParseException {
+        if (Session.users[Session.active_user_index].index> Session.users[Session.active_user_index].orders.length)
+            throw new ArrayIndexOutOfBoundsException("Amount of orders has been exceeded by user. Please remove an existing order and try again. ");
+        else {
+            System.out.println("Order request action");
+            System.out.println("\nPlease inform the following information requested...");
+
+            System.out.println("Order id: ");
+            int id=Input.readInt();
+
+            System.out.println("Order arrival date (MM/dd/yyyy): ");
+            String arrival_date = Input.readString();
+
+            Order o = new Order (id, arrival_date, Session.users[Session.active_user_index],Session.users[Session.active_user_index].getDepartment());
+            Session.users[Session.active_user_index].orders[index]=o;
+            index++;
+            return true;
+        }
+    }
