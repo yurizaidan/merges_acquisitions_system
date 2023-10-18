00011412362f096a148ba2fd23a036e312f0994b
Artur Lemos Pereira <abui.lepereira@gmail.com>
2023-10-17 21:27:50 -0300

    Input pronto

--- src/model/Input.java
+++ src/model/Input.java
@@ -1,3 +1,4 @@
+package model;
 import java.util.Scanner;
 
 public class Input {
@@ -14,6 +15,30 @@ public class Input {
         }
         int input=in.nextInt();
         in.nextLine();
-        return input
+        return input;
         }
+
+	public static String readString (){
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nEnter input: ");
+        System.out.println(":>");
+        return in.nextLine();
+    }
+
+    
+    public static double readDouble (){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\nEnter input: ");
+        System.out.println(":>");
+       
+        while(!in.hasNextDouble()){
+            in.next();
+            System.out.println("Invalid entry! Only double type allowed. Please try again: n:>");
+        }
+        
+        double input = in.nextDouble();
+        in.nextLine();
+        return input;
+    }
 }
