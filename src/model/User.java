1b181431ff04549f6c3d6ca84d9bf84cc3c432e4
Lucas Levandoski <lucasdelucas124@gmail.com>
2023-10-19 14:09:26 -0300

    Clase user concluida

--- src/model/User.java
+++ src/model/User.java
@@ -85,6 +85,13 @@ public static void removeOrderByCode() {
         }
     }
 
+public static void addAssetOrder() {
+        if (Order.index > Order.inventory.length)
+            throw new ArrayIndexOutOfBoundsException("Amount of assets has been exceeded by order. Please remove an existing asset and try again. ");
+        else {
+            Order.addAsset();
+        }
+    }
 
 public String getFirst_name() {
 
