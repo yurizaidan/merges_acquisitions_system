03e1ddd872a439b4bb919b173f925e96b180f010
Artur Lemos Pereira <abui.lepereira@gmail.com>
2023-10-19 13:36:32 -0300

    Metodo getOrderSumary pronto

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,25 +1,12 @@
-public static boolean addAsset (){
-        if (index>inventory.length) throw new ArrayIndexOutOfBoundsException("The inventory capacity has been exceeded. Please remove an existing asset already added to the inventory and try again.");
-        else {
-            System.out.println("Asset enrollment action");
-            System.out.println("\nPlease inform the following information requested...");
+public String getOrderSummary() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nOrder Summary: ");
+        sb.append("\nOrder id: "+this.getId());
+        sb.append("\nOrder arrival date (mm/dd/yyyy): "+this.getOrder_arrival_date());
+        sb.append("\nOrder sign off (mm/dd/yyyy): "+this.getOrder_signoff_date());
+        sb.append("\nOrder requester name: "+this.getUser().getFirst_name()+" "+this.getUser().getLast_name());
+        sb.append("\nDepartment: "+this.getDepartment().name);
+        sb.append("\nTotal order price (BRL): R$ "+this.getOrder_total_price());
 
-            System.out.println("Asset name: ");
-            String name = Input.readString();
-            System.out.println("Asset description: ");
-            String description = Input.readString();
-            System.out.println("Asset unit price (BRL): ");
-            double asset_unit_price = Input.readDouble();
-            System.out.println("Asset total amount: ");
-            int asset_total_amount = Input.readInt();
-            double asset_total_price=asset_total_amount*asset_unit_price;
-
-            order_total_price+=asset_total_price;
-
-            Asset a = new Asset(name, description, asset_unit_price, asset_total_amount, asset_total_price);
-            inventory[index]=a;
-            index++;
-            System.out.println("Asset has been successfully added to the inventory.");
-            return true;
-        }
+        return sb.toString();
     }
