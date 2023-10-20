f9c7b1cf992cd25b7beec9a4707402e5ef9bd996
Yuri Zaidan <yurizaidan@Yuris-MacBook-Pro.local>
2023-10-20 19:16:47 -0300

    Adding current files

--- src/model/Employee.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Employee extends User {
-
-    public Employee(String first_name, String last_name, int id, Department department) {
-        super(first_name, last_name, id, department);
-        this.employment_type="Employee";
-    }
-}
