67af91afab1b1a68765e7fa2d0db1771b196ae6e
Yuri Zaidan <yurizaidan@Yuris-MacBook-Pro.local>
2023-10-20 18:50:06 -0300

    Removing User.java file

--- User.java
+++
@@ -1,7 +0,0 @@
-public static void addAssetOrder() {
-        if (Order.index > Order.inventory.length)
-            throw new ArrayIndexOutOfBoundsException("Amount of assets has been exceeded by order. Please remove an existing asset and try again. ");
-        else {
-            Order.addAsset();
-        }
-    }
