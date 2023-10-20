4bb7a52ffd3996bfb6b643e759d0d608f3db2db4
Yuri Zaidan <yurizaidan@Yuris-MacBook-Pro.local>
2023-10-20 18:56:25 -0300

    Adding changes to class Main.java

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import model.Application;
+
+import java.text.ParseException;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Application app = new Application();
+
+        app.runApp();
+    }
+}
